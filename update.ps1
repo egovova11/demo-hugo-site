6ae6999bb71d24435fca96b5b5708e0b14225c96
Vladimir Doroshin <abgvector@gmail.com>
2019-04-03 22:44:59 +0300

    public files

---
+++ update.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host "Deploying updates to GitHub..."
+
+# Build the project
+#hugo
+& hugo -t 'spectrum'
+
+cd public
+
+& git add .
+
+$msg = "rebuilding site $(Get-Date)"
+
+& git commit -m $msg
+
+& git push origin master
+
+cd ..
